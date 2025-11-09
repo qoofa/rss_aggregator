4a256a41359b4f38ed306e8d1601478095bfb014
Minhaj <mdminhajgdr@gmail.com>
2025-11-09 17:10:00 +0530

    feat: create feed endpoint

--- models.go
+++ models.go
@@ -24,3 +24,23 @@ func databaseUserTOUser(u database.User) User {
 		ApiKey:    u.ApiKey,
 	}
 }
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url       string    `json:"url"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func databaseFeedToFeed(f database.Feed) Feed {
+	return Feed{
+		ID:        f.ID,
+		CreatedAt: f.CreatedAt,
+		UpdatedAt: f.UpdatedAt,
+		Name:      f.Name,
+		Url:       f.Url,
+		UserID:    f.UserID,
+	}
+}
